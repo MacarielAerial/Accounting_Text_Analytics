f39749cd75bc83d50193dc76b63f006d3b9e2eda
Yifei Yu <Chris@webredir-41-167.london.edu>
2020-03-10 15:47:12 +0000

    submitted version

--- config.py
+++ config.py
@@ -1,10 +1,1 @@
-'''
-This is the configueration file for the main module analysis.py
-'''
-import os
 
-class Config:
-	'''Provides the programme with an input-output pipeline'''
-	def dir_create(path):
-		if not os.path.exists(path):
-			os.makedirs(path)
